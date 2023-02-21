9ec63e153e707f70a4e5c81902e8bb51f6cf9680
bconsolvo <benjamin.consolvo@gmail.com>
2023-02-21 12:08:10 -0800

    v7 train

---
+++ cresi/v7_train_devcloud.sh
@@ -0,0 +1,16 @@
+# train
+
+#set env vars
+export OMP_NUM_THREADS=24
+#export NUM_PROCESSES=8
+#export NUM_PROCESSES_PER_NODE=2
+#export CCL_WORKER_COUNT=2
+#export CCL_WORKER_AFFINITY=auto
+export KMP_BLOCKTIME=50
+#export KMP_HW_SUBSET=1T
+#export KMP_HW_SUBSET=
+export oneDNN_VERBOSE=1
+
+#echo $KMP_HW_SUBSET
+#python /home/devcloud/.conda/envs/py39/lib/python3.9/site-packages/intel_extension_for_pytorch/cpu/launch.py --distributed 01_train.py configs/ben/v7_devcloud_optimize_baseline_ben.json --fold=0
+python 01_train.py configs/ben/v7_devcloud_optimize_baseline_ben.json --fold=0
